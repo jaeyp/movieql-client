4b30a4abfb26dd7753cc959a176600a64404ab5d
jaey p <jp.inseoul@gmail.com>
2020-02-19 04:06:58 -0500

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from "../routes/Home"
+import Detail from "../routes/Detail"
 
 function App() {
   return (
-    <div className="App"></div>
+    <HashRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
