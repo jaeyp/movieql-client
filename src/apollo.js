8232a6441f249c2d4af82afe485648d7f1f056a6
jaey p <jp.inseoul@gmail.com>
2020-02-19 04:49:47 -0500

    getting movie list

---
+++ src/apollo.js
@@ -0,0 +1,7 @@
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+    uri: 'https://movieql-server.now.sh/graphql',
+});
+
+export default client;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,24 @@
-import React from "react";
-export default () => <h1>Home</h1>;
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks'
+
+const GET_MOVIES = gql`{
+    list(sort_by:"popularity.desc", page:1) {
+        id
+        title
+        poster_path
+    }
+}`
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES)
+
+    console.log(loading, error, data)
+
+    if (loading) {
+        return "loading..."
+    }
+    if (data && data.list) {
+        return data.list.map(item => <h1>{item.title}</h1>)
+    }
+};
