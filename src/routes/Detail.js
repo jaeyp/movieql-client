ff00f9c9c83ce2c36539bed87d76d76b55fa1cc4
jaey p <jp.inseoul@gmail.com>
2020-02-20 02:45:30 -0500

    added cast info

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -18,8 +18,14 @@ const GET_DETAILS = gql`
       release_date
       runtime
     }
+    credits(movie_id: $id) {
+      id
+      character
+      name
+      profile_path
+    }
   }
-`;
+`
 
 export default () => {
   const { id } = useParams();
@@ -29,13 +35,16 @@ export default () => {
   return (
     <Container>
       <Column>
-        <Title>{loading ? "Loading..." : data.details.title}</Title>
-        {!loading && data.details && (
-          <>
-            <Subtitle>Popularity: {Math.floor(data.details.popularity)} · Rate: {data.details.vote_average}</Subtitle>
-            <Description>{data.details.overview}</Description>
-          </>
-        )}
+        {loading ? <Title>"Loading..."</Title> :
+          data.details && (
+            <>
+              <Title style={data.details.title.length < 20 ? { fontSize: '65px' } : { fontSize: '45px' }}>{data.details.title}</Title>
+              <Subtitle>Rate: {data.details.vote_average}</Subtitle>
+              <Subtitle>Popularity: {Math.floor(data.details.popularity)}</Subtitle>
+              {data.credits && <Cast>{data.credits.slice(0, 4).map((item, i) => i < 3 ? `${item.name}, ` : `${item.name}`)}</Cast>}
+              <Description>{data.details.overview}</Description>
+            </>
+          )}
       </Column>
       <Poster bg={data && `${IMAGE_PATH}${data.details.poster_path}`} />
     </Container>
@@ -59,12 +68,22 @@ const Column = styled.div`
 
 const Title = styled.h1`
   font-size: 65px;
+  font-weight: 500;
   margin-bottom: 15px;
 `;
 
 const Subtitle = styled.h4`
-  font-size: 35px;
-  margin-bottom: 10px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #252829;
+  margin-bottom: 4px;
+`;
+
+const Cast = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  color: #252829;
+  margin-bottom: 20px;
 `;
 
 const Description = styled.p`
@@ -72,8 +91,9 @@ const Description = styled.p`
 `;
 
 const Poster = styled.div`
+  margin-right: 10px;
   width: 25%;
-  height: 60%;
+  height: 70%;
   /*border-radius: 14px;*/
   background-color: transparent;
   background-image: url(${props => props.bg});
